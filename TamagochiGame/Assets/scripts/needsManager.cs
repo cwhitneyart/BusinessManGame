050553fbeedcc9f24f6f4e43c6a5b6c2549cc990
whitney Constantine <constantinewhitney@gmail.com>
2017-02-08 12:49:08 +1000

    added barebones UI menu

---
+++ TamagochiGame/Assets/scripts/needsManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class needsManager : MonoBehaviour
+{
+
+    public GameObject needsWindow;
+
+
+    public void needsWindowActivation()
+    {
+        if (needsWindow.activeInHierarchy == false)
+        {
+            needsWindow.SetActive(true);
+        }
+        else if (needsWindow.activeInHierarchy == true)
+        {
+            needsWindow.SetActive(false);
+        }
+       
+       
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        needsWindow.SetActive(false);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- TamagochiGame/Assets/scripts/player/playerNeeds.cs
+++ TamagochiGame/Assets/scripts/player/playerNeeds.cs
@@ -1,16 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//This class will look after all the needs the player's businessman has. It will alert the player when a certain need is too low. 
+public class playerNeeds : MonoBehaviour
+{
+    public float hungerNeed;
+    public float thirstNeed;
+    public float sleepNeed;
+    public float loveNeed;
 
-public class playerNeeds : MonoBehaviour {
+    float workDone;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkHunger();
+        checkThirst();
+        checksleep();
+    }
+
+    void checkHunger()
+    {
+        //check player's hunger level
+    }
+
+    void checkThirst()
+    {
+        //check player's thirst
+    }
+
+    void checksleep()
+    {
+        //check player's sleep
+    }
 }
