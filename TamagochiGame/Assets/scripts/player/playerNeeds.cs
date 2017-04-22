0d21c7ebabc7e4a5eb0c77ae53490c42c44248ce
whitney Constantine <constantinewhitney@gmail.com>
2017-04-22 22:14:27 +1000

    Added needsManger.cs Added barebones UI with sliders that are effected by player needs added rudimentary warning when business man is hungry hunger and sleep now slowly decrease over time. This can be hastened or slowed in future easily

--- TamagochiGame/Assets/scripts/player/playerNeeds.cs
+++ TamagochiGame/Assets/scripts/player/playerNeeds.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 //This class will look after all the needs the player's businessman has. It will alert the player when a certain need is too low. 
 public class playerNeeds : MonoBehaviour
 {
-    public float hungerNeed;
-    public float thirstNeed;
-    public float sleepNeed;
-    public float loveNeed;
+    public float hungerNeed = 1;
+    public float thirstNeed = 1;
+    public float sleepNeed = 1;
+    public float loveNeed = 1;
+
+    public float sleepDecreaseRate = 1f;
+    public float thirstDecreaseRate = 1f;
+    public float hungerDecreaseRate = 1f;
+
+    public Slider hungerSlider;
+    public Slider sleepSlider;
+
+    bool hungerWarningCalled = false;
+
+    #region
+    public GameObject hungerWarningText;
+    #endregion
 
     float workDone;
 
     // Use this for initialization
     void Start()
     {
+        InvokeRepeating("decreaseSleep", 2f, 1f);
+        InvokeRepeating("decreasesHunger", 2f, 1f);
 
+        hungerWarningCalled = false;
     }
 
     // Update is called once per frame
@@ -23,11 +40,48 @@ public class playerNeeds : MonoBehaviour
         checkHunger();
         checkThirst();
         checksleep();
+        hungerSlider.value = hungerNeed;
+        sleepSlider.value = sleepNeed;
     }
+
+
+    void decreaseSleep()
+    {
+        sleepNeed -= sleepDecreaseRate; //* Time.deltaTime;
+    }
+
+    void decreasesHunger()
+    {
+     
+            hungerNeed -= hungerDecreaseRate; //* Time.deltaTime;
+        
+   
+    }
+
+
 
     void checkHunger()
     {
-        //check player's hunger level
+        if(hungerWarningCalled == false)
+        {
+            //check player's hunger level
+            if (hungerNeed <= 0.5)
+            {
+                StartCoroutine(hungerWarning());
+            }
+        }
+     
+
+       
+    }
+
+    IEnumerator hungerWarning()
+    {
+        hungerWarningText.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        hungerWarningText.SetActive(false);
+        hungerWarningCalled = true;
+      
     }
 
     void checkThirst()
@@ -38,5 +92,6 @@ public class playerNeeds : MonoBehaviour
     void checksleep()
     {
         //check player's sleep
+       
     }
 }
